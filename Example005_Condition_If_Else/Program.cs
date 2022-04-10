3f4ad11d2872934826418e2fab3beb9e41ed262a
vladshkrebko <vladshkrebko@gmail.com>
2022-04-10 22:54:57 +0300

    add Example005_Condition_If_Else

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "влад")
+{
+    Console.WriteLine("Ура, это же Влад");
+}
+else
+{
+    Console.Write("Привет пользователь, ");
+    Console.WriteLine(username);
+}
